24cd7cd0ceb08c896e9b350668baa73f2997b220
Andres Pavez <apavez.zavala@gmail.com>
2018-01-05 18:10:10 -0800

    Fixed /etc/shadow
    Fixed /etc/shadow for https://bugs.debian.org/857803

---
+++ tools/hooks/06-fix-shadow.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Thanks to https://git-tails.immerda.ch/tails/tree/config/chroot_local-hooks
+
+set -x
+set -e
+set -u
+
+# Post-process /etc/shadow by setting the sp_lstchg field to the number of days
+# since SOURCE_DATE_EPOCH instead of 1st Jan 1970. (#12339)
+# XXX:Buster: drop this if https://bugs.debian.org/857803 is fixed.
+
+cut -d: -f1 $WD/chroot/etc/shadow | \
+  xargs -L1 \
+    chage --lastday \
+      "$(($(date --utc --date "@${SOURCE_DATE_EPOCH}" "+%s") / 86400))"
+
+# END
