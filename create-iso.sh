d64261f0dc8188c86d90a312399971204a232b00
Andres Pavez <apavez.zavala@gmail.com>
2018-05-10 16:21:07 -0700

    Fixed typo
    Fixed typo

--- create-iso.sh
+++ create-iso.sh
@@ -1,4 +1,4 @@
-coen#!/bin/bash
+#!/bin/bash
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
